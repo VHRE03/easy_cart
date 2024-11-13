ff74823a36fb815240afa7916dd0c1dc46218127
VHRE03 <vhre03@gmail.com>
2024-11-12 20:26:46 -0600

    Creación del frontend

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'payment_method', 'payment_date', 'order')
